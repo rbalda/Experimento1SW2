7c776f3c5f03f9fe828e616e0dff11ccb985ebf0
Cliente CONTROL-PC <Cliente CONTROL-PC>
2016-07-12 16:40:20 -0500

    tests prestamo

---
+++ tests/tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+from prestamo import Prestamo
+
+
+class prestamoTest(unittest.TestCase):
+
+    def test_calcular_valor1(self):
+        self.prestamo = Prestamo.objects.create(4000,3)
+        self.calcularValor()
+        self.assertIsInstance(self.prestamo,Prestamo)
+
+    def test_calcular_valor2(self):
+        self.prestamo = Prestamo.objects.create(6000,6)
+        self.calcularValor()
+        self.assertIsInstance(self.prestamo,Prestamo)
+
+    def test_calcular_valor3(self):
+        self.prestamo = Prestamo.objects.create(10000,12)
+        self.calcularValor()
+        self.assertIsInstance(self.prestamo,Prestamo)
+
+    def test_calcular_valor4(self):
+        self.prestamo = Prestamo.objects.create(30000,13)
+        self.calcularValor()
+        self.assertIsInstance(self.prestamo,Prestamo)
+
+    def test_calcular_valor5(self):
+        self.prestamo = Prestamo.objects.create(30000,-1)
+        self.calcularValor()
+        self.assertIsInstance(self.prestamo,Prestamo)
+
+    def test_calcular_valor6(self):
+        self.prestamo = Prestamo.objects.create(40000,14)
+        self.calcularValor()
+        self.assertIsInstance(self.prestamo,Prestamo)
+
+
+
+
+
+
+
+
