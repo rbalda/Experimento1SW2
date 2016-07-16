ef869fa0bb5b84bb16c5da53dc01041790f39180
Rene Balda <rbalda@espol.edu.ec>
2016-07-15 21:49:40 -0500

    fix indent

--- prestamo.py
+++ prestamo.py
@@ -16,6 +16,6 @@ class Prestamo():
             capital_total=float(prestamo+comision)
             interes=float(((capital_total*1.5)/100)*tiempo)
             return float(capital_total+interes)
-		if(prestamo > 20000):
+        if(prestamo > 20000):
 		    print("El banco no puede otorgar dicho prestamo ya que excede al monto definido")
 			return None
