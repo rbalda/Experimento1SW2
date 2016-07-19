9e1ceef86084c4b4cd9178fa231b4242f477d9f7
Leonardo Eras <leoded94@hotmail.com>
2016-07-19 16:30:21 -0500

    Update test.py
    Corrección de test

--- Files/test.py
+++ Files/test.py
@@ -8,23 +8,23 @@ class SimpleTestCase(unittest.TestCase):
 
     def testA(self):
         #Prueba ejemplo de la hoja
-        assert Prestamo.valor_total(self, 2000, 2) == 2080.8
+        assert self.prestamo.valor_total(2000, 2) == 2080.8
 
     def testB(self):
         #Prueba con capital menor a 5000, 3 meses
-        assert Prestamo.valor_total(self, 4999, 3) == 5251.9493999999995
+        assert self.prestamo.valor_total(4999, 3) == 5251.9493999999995
 
     def testC(self):
         #Prueba con capital entre 5000 a 10000, 5 meses
-        assert Prestamo.valor_total(self, 8000, 5) == 8755
+        assert self.prestamo.valor_total(8000, 5) == 8755
 
     def testD(self):
         #Prueba con capital entre 10000 y 20000, 10 meses
-        assert Prestamo.valor_total(self, 12000, 10) == 14352
+        assert self.prestamo.valor_total(12000, 10) == 14352
 
     def testE(self):
         #Prueba con capital mayor a 20000, 5 meses
-        assert Prestamo.valor_total(self, 20001, 5) == None
+        assert self.prestamo.valor_total(20001, 5) == None
 
 if __name__ == "__main__":
     unittest.main()
