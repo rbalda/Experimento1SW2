4497ac3e48b16bebb4ec0eaec5ebef74715fc641
Wilson <wesquall@hotmail.com>
2016-07-19 16:39:04 -0500

    eliminando carpeta cache

--- deberEnriquez/SimpleTestCase.py
+++ deberEnriquez/SimpleTestCase.py
@@ -1,5 +1,6 @@
 import unittest
 import Prestamo # code from module you're testing
+from django.test import TestCase
 
 
 class SimpleTestCase(unittest.TestCase):
@@ -15,6 +16,14 @@ class SimpleTestCase(unittest.TestCase):
     def test_caso3(self):
 		prestamo = Prestamo()
 		self.assertEqual(prestamo.valor_total(22000,5), None)
+
+    def test_caso4(self):
+		prestamo = Prestamo()
+		self.assertEqual(prestamo.valor_total(15000,20), None)
+	
+	def test_caso5(self):
+		prestamo = Prestamo()
+		self.assertEqual(prestamo.valor_total(6000,7), None)
 		
 		
 if __name__ == '__main__':
