537e5d28a0beba6146dff2c1eac7399c429a4bc9
CltControl <jayala@fiec.espol.edu.ec>
2016-07-19 16:02:56 -0500

    Modificados casos de prueba y condiciones de prestamo

--- deberEnriquez/Prestamo.py
+++ deberEnriquez/Prestamo.py
@@ -1,21 +1,21 @@
 class Prestamo():
         
         def valor_total(self,prestamo,tiempo):
-                if(prestamo < 5000):
+                if(prestamo < 5000 and prestamo > 0):
                                 comision=(prestamo *2)/100
                                 capital_total=float(prestamo+comision)
                                 interes=float(((capital_total*1)/100)*tiempo)
                                 return float(capital_total+interes)
-                if(prestamo > 5000 & prestamo < 9999.99):
+                if(prestamo > 5000 and prestamo < 9999.99):
                                 comision=(prestamo*3)/100
                                 capital_total=float(prestamo+comision)
                                 interes=float(((capital_total*1.25)/100)*tiempo)
                                 return float(capital_total+interes)
-                if(prestamo > 5000 & prestamo < 20000):
+                if(prestamo > 5000 and prestamo < 20000):
                                 comision=(prestamo*4)/100
                                 capital_total=float(prestamo+comision)
                                 interes=float(((capital_total*1.5)/100)*tiempo)
                                 return float(capital_total+interes)
-                if(prestamo > 20000):
+                if(prestamo > 20000 or prestamo < 0):
                                 print("El banco no puede otorgar dicho prestamo ya que excede al monto definido")
                                 return None
--- deberEnriquez/SimpleTestCase.py
+++ deberEnriquez/SimpleTestCase.py
@@ -4,31 +4,18 @@ import Prestamo # code from module you're testing
 
 class SimpleTestCase(unittest.TestCase):
 
-    def setUp(self):
-        """Call before every test case."""
-        self.prestamo = Prestamo()
-        self.valor_total = open(self, 2000, 2 )
-
-    def tearDown(self):
-        """Call after every test case."""
-        self.prestamo = None
-
-    def testA(self):
-        """Test case A. note that all test method names must begin with 'test.'"""#VERIFICA para $2000 a 2 años.
-        assert prestamo.valor_total(prestamo, 2000, 2) == 543, "valor_total() not calculating values correctly"
-
-    def testB(self):
-        """test case B"""
-        assert prestamo.valor_total(prestamo, 8000, 4) == 543, "valor_total() not calculating values correctly"
-
-    def testC(self):
-        """test case C"""
-        assert prestamo.valor_total(prestamo, 15000, 5) == 543, "valor_total() not calculating values correctly"
-
-    def testD(self):
-        """test case C"""
-        assert prestamo.valor_total(prestamo, 23000, 2) == 543, "valor_total() not calculating values correctly"
-
-
-if __name__ == "__main__":
+    def test_caso1(self):
+		prestamo = Prestamo()
+		self.assertEqual(prestamo.valor_total(-10,2), None)
+		
+    def test_caso2(self):
+		prestamo = Prestamo()
+		self.assertEqual(prestamo.valor_total(2000,2), 2080.8)
+		
+    def test_caso1(self):
+		prestamo = Prestamo()
+		self.assertEqual(prestamo.valor_total(22000,5), None)
+		
+		
+if __name__ == '__main__':
     unittest.main() # run all tests
