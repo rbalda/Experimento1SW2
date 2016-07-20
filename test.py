e65bfaff2f4b99cf228dc554536388f873de0054
Kattya Desiderio <desideri@espol.edu.ec>
2016-07-19 20:16:30 -0500

    *

--- test.py
+++ test.py
@@ -4,22 +4,18 @@ class TestPrestamo(unittest.TestCase):
     def test_valor1(self):
 		p = Prestamo()
 		""" Inferior a 5000 y tiempo inferior a 3 meses"""
-        total = p.valor_total(1000,2)
-		self.assertEqual(total, 1040.4)
+        self.assertEqual(p.valor_total(1000,2), 1040.4)
     def test_valor2(self):
 		p = Prestamo()
         """ Entre 5000 y 9999.99 y tiempo inferior a 6 meses"""
-        total = p.valor_total(5000,2)
-		self.assertEqual(total, 5278.75)
+        self.assertEqual(p.valor_total(5000,2), 5278.75)
     def test_valor3(self):
 		p = Prestamo()
 		""" Entre 10000 y 20000 y tiempo inferior a 12 meses"""
-        total = p.valor_total(10000,2)
-		self.assertEqual(total, 10712)
+        self.assertEqual(p.valor_total(10000,2), 10712)
     def test_valor4(self):
 		p = Prestamo()
 		""" mayor a 20000 """
-        total = p.valor_total(30000,2)
-		self.assertEqual(total, None)
+        self.assertEqual(p.valor_total(30000,2), None)
 if __name__ == '__main__':
     unittest.main()
