e55301c98676a1c9cba10bb3569471e7fdab5e2c
Diego <dgrockp@gmail.com>
2016-07-20 00:28:15 -0500

    Corregido codigo en los tests

--- prestamo.py
+++ prestamo.py
@@ -2,23 +2,22 @@
 class Prestamo():
 	
 	def valor_total(self,prestamo,tiempo):
-		if(tiempo<=0 or tiempo>12):
-				return None
-		if(prestamo<5000):
+		
+		if(prestamo<5000 and tiempo<= 3 and tiempo>=0):
 				comision=(prestamo *2)/100
 				capital_total=float(prestamo+comision)
 				interes=float(((capital_total*1)/100)*tiempo)
 				return float(capital_total+interes)
-		if(prestamo>= 5000 and prestamo<=9999.99):
+		if(prestamo>= 5000 and prestamo<=9999.99 and tiempo <= 6):
 				comision=(prestamo*3)/100
 				capital_total=float(prestamo+comision)
 				interes=float(((capital_total*1.25)/100)*tiempo)
 				return float(capital_total+interes)
-		if(prestamo>= 10000 and prestamo<=20000):
+		if(prestamo>= 10000 and prestamo<=20000 and tiempo<=6):
 				comision=(prestamo*4)/100
 				capital_total=float(prestamo+comision)
 				interes=float(((capital_total*1.5)/100)*tiempo)
 				return float(capital_total+interes)
-		if(prestamo>20000):
+		if(prestamo>20000 and tiempo>0):
 				print("El banco no puede otorgar dicho prestamo ya que excede al monto definido")
 				return None
--- tests.py
+++ tests.py
@@ -9,34 +9,16 @@ class prestamoTestCase(unittest.TestCase):
         self.prestamo = Prestamo()
 
     def test_calcular_valor1(self):
-        self.prestamo = Prestamo.objects.create(4000,3)
-        self.calcularValor()
-        self.assertEquals(self.prestamo.valor_total(4000,3),Prestamo)
+        self.assertEquals(self.prestamo.valor_total(2000,2),2080.8)
 
     def test_calcular_valor2(self):
-        self.prestamo = Prestamo.objects.create(6000,6)
-        self.calcularValor()
-        self.assertIsInstance(self.prestamo,Prestamo)
+        self.assertEquals(self.prestamo.valor_total(5008,2),5286.95)
 
     def test_calcular_valor3(self):
-        self.prestamo = Prestamo.objects.create(10000,12)
-        self.calcularValor()
-        self.assertIsInstance(self.prestamo,Prestamo)
+        self.assertEquals(self.prestamo.valor_total(12008,3),12862.64)
 
     def test_calcular_valor4(self):
-        self.prestamo = Prestamo.objects.create(30000,13)
-        self.calcularValor()
-        self.assertIsInstance(self.prestamo,Prestamo)
-
-    def test_calcular_valor5(self):
-        self.prestamo = Prestamo.objects.create(30000,-1)
-        self.calcularValor()
-        self.assertIsInstance(self.prestamo,Prestamo)
-
-    def test_calcular_valor6(self):
-        self.prestamo = Prestamo.objects.create(40000,14)
-        self.calcularValor()
-        self.assertIsInstance(self.prestamo,Prestamo)
+        self.assertEquals(self.prestamo.valor_total(30000,7),None)
 
 
 if __name__ == "__main__":
