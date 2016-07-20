3408b4ed5655f5ec925825dff0c06ed5a9ea2f53
JaninaCosta <jany_costa90@hotamil.com>
2016-07-19 22:38:39 -0500

    Coverage

--- codigo.py
+++ codigo.py
@@ -1,6 +1,5 @@
 class Prestamo():
 	def valor_total(self,prestamo,tiempo):
-		# 2% comision 1% interes 3 meses
 		if(prestamo>0 and tiempo>0):
 			if(prestamo<5000 and tiempo<=3):
 				comision=(prestamo *2)/100
@@ -12,7 +11,6 @@ class Prestamo():
 				print("%de comision:", comision)
 				print ("%de interes:", interes)
 				return float(capital_total+interes)
-			# 3% comision 1.25% interes 6 meses
 			elif(prestamo<10000 and tiempo<=6):
 				comision=(prestamo*3)/100
 				capital_total=float(prestamo+comision)
@@ -23,7 +21,6 @@ class Prestamo():
 				print("%de comision:", comision)
 				print ("%de interes:", interes)
 				return float(capital_total+interes)
-			# 4% comision 1.5% interes 12 meses
 			elif(prestamo<20000 and tiempo<=12):
 				comision=(prestamo*4)/100
 				capital_total=float(prestamo+comision)
--- prueba.py
+++ prueba.py
@@ -11,15 +11,21 @@ class AddTest(unittest.TestCase):
       pass
    def test_prestamoMenorCero(self):
        self.assertEqual(self.prestamo.valor_total(-1,1),None)
+   def test_prestamoMenorCeroDos(self):
+       self.assertEqual(self.prestamo.valor_total(0,1),None)
    def test_tiempoMenorCero(self):
        self.assertEqual(self.prestamo.valor_total(300,-1),None)
    def test_prestamoMenor5000(self):
        self.assertEqual(self.prestamo.valor_total(3400,3),3572.04)
+   def test_prestamoMenor5000Dos(self):
+       self.assertEqual(self.prestamo.valor_total(4099,3),4306.4094)
    def test_prestamoMenor10000(self):
        self.assertEqual(self.prestamo.valor_total(9000,4),9733.5)
    def test_prestamoMenor20000(self):
        self.assertEqual(self.prestamo.valor_total(17800,10),21288.8)
    def test_prestamoMayor20000(self):
        self.assertEqual(self.prestamo.valor_total(20001,12),None)
+   def test_prestamoMMayor20000(self):
+       self.assertEqual(self.prestamo.valor_total(50000,11),None)
 if __name__=='__main__':
    unittest.main()
