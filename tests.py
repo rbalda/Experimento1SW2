9f41dc0b17ed321766fc16f008db4a402efc84ae
Diego <dgrockp@gmail.com>
2016-07-26 15:44:34 -0500

    corregido el valor esperado en el assert

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ class prestamoTestCase(unittest.TestCase):
         self.assertEquals(self.prestamo.valor_total(5008,2),5286.95)
 
     def test_calcular_valor3(self):
-        self.assertEquals(self.prestamo.valor_total(12008,3),12862.64)
+        self.assertEquals(self.prestamo.valor_total(12008,2),12862.64)
 
     def test_calcular_valor4(self):
         self.assertEquals(self.prestamo.valor_total(30000,7),None)
